2a3ec518d575fca04b77a77e9053819b7bc102d1
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-09-26 10:42:39 +0100

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,39 @@ import { Footer } from "./components/Footer";
 import {   Test } from "./components/hero";
 import { Projects } from "./components/project";
 import { Stack } from "./components/stack";
+import { useState, useEffect } from "react";
+
+const Loader = () => (
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <div className="text-center">
+    <div className="loader" role="status">
+</div>
+    </div>
+  </div>
+  
+  
+  );
+  
+  const Layout = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      const delayLoader = setTimeout(() => {
+        setIsLoading(false);
+      }, 5000);
+  
+      return () => clearTimeout(delayLoader);
+    }, []);
+  
+    return isLoading ? <Loader /> : children;
+  };
 
 function App() {
+  
   return (
 <div>
+<Layout>
+
 
   <Test />
  
@@ -15,6 +44,7 @@ function App() {
    
   <About/>
   <Footer />
+  </Layout>
 </div>
   );
 }
