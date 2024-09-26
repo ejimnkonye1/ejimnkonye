4cc763528c9621d9353e84c219bc5ae37e0ac238
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-09-26 15:45:07 +0100

    animation

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,18 +3,23 @@
 import img from '../assets/background.png'
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdOutlineCancel } from "react-icons/md";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import 'aos/dist/aos.css';
+import Aos from 'aos'
 
 
 function Test() {
+  useEffect(() => {
+    Aos.init()
+     },[])
   const [isOpen, setIsOpen] = useState(false); // State to manage menu visibility
 
   const toggleMenu = () => {
     setIsOpen(!isOpen); // Toggle menu state
   };
     return(
-      <section className="relative hero text-white overflow-hidden  " style={{height:'29rem', width:'100%'}}>
-        <nav className="">
+      <section className="relative hero text-white overflow-hidden  " style={{height:'29rem', width:'100%'}} data-aos="fade-up"  data-aos-delay="0"  data-aos-duration="2000">
+        <nav className="" data-aos="zoom-in" data-aos-delay="2000" >
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
       
@@ -91,24 +96,24 @@ function Test() {
     </nav>
       </header> */}
       <div className="hero-inner flex flex-col items-center justify-center text-center p-8 text-white">
-        <p className="sub-heading text-lg">Hey, I'm Ejimnkonye-Onyedika. I am a</p>
-        <div className="hero-heading">
-          <h1 className="text-4xl font-bold">FRONTEND WEB DEVELOPER</h1>
-          
-        </div>
-        <p className="hero-intro mt-4 text-lg max-w-lg">
-          Mainly building with ReactJS, I focus on turning ideas into functional and visually stunning realities. Let's collaborate and create something remarkable together!
-        </p>
-        <div className="hero-ctas mt-6 space-x-4">
-          <a href="#projects">
-            <button className="bg-white text-blue-600 px-4 py-2 rounded-lg">My Projects</button>
-          </a>
-          <a download href="/assets/resume.6054d56e.pdf">
-            <button className="bg-white text-blue-600 px-4 py-2 rounded-lg">Resume</button>
-          </a>
-        </div>
-      </div>
-      <img src={img} alt="Background pattern" className="pattern absolute bottom-0 top-0 left-0  h-auto object-cover opacity-20" style={{maxWidth:'100%', width:'100%'}} />
+  <p className="sub-heading text-lg" data-aos="fade-up" data-aos-delay="400"  data-aos-duration="3000">Hey, I'm Ejimnkonye-Onyedika. I am a</p>
+  <div className="hero-heading">
+    <h1 className="text-4xl font-bold" data-aos="fade-up" data-aos-delay="500"  data-aos-duration="1000">FRONTEND WEB DEVELOPER</h1>
+  </div>
+  <p className="hero-intro mt-4 text-lg max-w-lg" data-aos="fade-up" data-aos-delay="1000"  data-aos-duration="3600">
+    Mainly building with ReactJS, I focus on turning ideas into functional and visually stunning realities. Let's collaborate and create something remarkable together!
+  </p>
+  <div className="hero-ctas mt-6 space-x-4">
+    <a href="#projects">
+      <button className="bg-white text-blue-600 px-4 py-2 rounded-lg" data-aos="flip-right" data-aos-delay="1000">My Projects</button>
+    </a>
+    <a download href="/assets/resume.6054d56e.pdf">
+      <button className="bg-white text-blue-600 px-4 py-2 rounded-lg" data-aos-delay="1000">Resume</button>
+    </a>
+  </div>
+</div>
+      <img src={img} alt="Background pattern" className="pattern absolute bottom-0 top-0 left-0  h-auto object-cover opacity-20" style={{maxWidth:'100%', width:'100%'}} data-aos="fade-up"
+     data-aos-anchor-placement="center-bottom" data-aos-delay="1000" />
     </section>
     
     )
